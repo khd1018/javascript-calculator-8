86cf1f901498bb3adfe2015e3fd4faddf818b5da
hyeongdong <khd941018@gmail.com>
2025-10-18 18:19:25 +0900

    test(Calculator): Calculator의 sum메서드 테스트 코드 구현

---
+++ __tests__/calculatorTest.js
@@ -0,0 +1,11 @@
+describe("Calculator test", () => {
+  const calculator = new Calculator();
+
+  test.each([
+    [[1, 2, 3], 6],
+    [[3, 4, 5], 12],
+    [[3, 4, 5], 12],
+  ])("숫자들의 합을 구할 수 있다.", (numbers, sumResult) => {
+    expect(calculator.sum(numbers)).toBe(sumResult);
+  });
+});

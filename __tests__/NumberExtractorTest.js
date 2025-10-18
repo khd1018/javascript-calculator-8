f362d66610b5cdaff6fd4c6f9a8aa83af1567d94
hyeongdong <khd941018@gmail.com>
2025-10-19 00:20:19 +0900

    test(NumberExtractor) : 콜론 구분자로 숫자 구분 확인하는 파라미터 추가

--- __tests__/NumberExtractorTest.js
+++ __tests__/NumberExtractorTest.js
@@ -3,6 +3,7 @@ describe("NumberExtractor test", () => {
 
   test.each([
     ["1,2,3", [1, 2, 3]],
+    ["3:5,7", [3, 5, 7]],
     ["1;3,5", [1, 3, 5]],
   ])("문자열에서 숫자들을 분리할 수 있다.", (strings, numbers) => {
     expect(numberExtractor.extract(strings)).toStrictEqual(numbers);

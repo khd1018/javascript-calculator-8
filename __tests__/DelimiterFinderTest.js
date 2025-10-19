80a006988c2ffa1ac7efe63b9e992635216d99e6
hyeongdong <khd941018@gmail.com>
2025-10-19 20:53:48 +0900

    test(DelimiterFinder): 커스텀 구분자가 없는 경우 테스트 추가

--- __tests__/DelimiterFinderTest.js
+++ __tests__/DelimiterFinderTest.js
@@ -10,4 +10,8 @@ describe("DelimiterFinder test", () => {
   ])("커스텀 구분자를 구할 수 있다.", (string, delimiter) => {
     expect(delimiterFinder.getDelimiter(string)).toStrictEqual(delimiter);
   });
+
+  test.each([["//\\n1,2:3"], ["1,2:3"]])("커스텀 구분자를 입력하지 않았는지 알 수 있다.", (string) => {
+    expect(delimiterFinder.getDelimiter(string)).toBeFalsy();
+  });
 });

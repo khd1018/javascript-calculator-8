b141109b81156d38a782fc045d1b2b7c61bcf570
hyeongdong <khd941018@gmail.com>
2025-10-20 03:35:42 +0900

    feat(App): 필요한 객체 생성 및 실행에 필요한 코드 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import InputView from "./views/InputView.js";
+import OutputView from "./views/outputView.js";
+import Calculator from "./services/Calculator.js";
+import DelimiterFinder from "./services/DelimiterFinder.js";
+import NumberExtractor from "./services/NumberExtractor.js";
+import InputValidator from "./services/InputValidator.js";
+import CalculatorController from "./controllers/CalculatorController.js";
+
 class App {
-  async run() {}
+  async run() {
+    const inputView = new InputView();
+    const outputView = new OutputView();
+    const calculator = new Calculator();
+    const delimiterFinder = new DelimiterFinder();
+    const numberExtractor = new NumberExtractor();
+    const inputValidator = new InputValidator();
+    const calculatorController = new CalculatorController(calculator, delimiterFinder, numberExtractor, inputValidator);
+
+    const userInputString = await inputView.getUserInput();
+    outputView.printResult(calculatorController.calculateSum(userInputString));
+  }
 }
 
 export default App;

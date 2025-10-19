0fceeab2b490037df9d0fb732e2b973708b378aa
hyeongdong <khd941018@gmail.com>
2025-10-20 01:12:11 +0900

    refactor(NumberExtractor): 초기화 함수 대신 add()에서 커스텀문자열을 저장하도록 변경
    초기화 함수에서 커스텀 문자열을 할당하게 되면 해당 클래스의 객체를
    생성할 때 제약이 있기 때문에 add()로 분리

--- src/services/NumberExtractor.js
+++ src/services/NumberExtractor.js
@@ -1,14 +1,8 @@
 class NumberExtractor {
   #delimiters = [":", ","];
 
-  constructor(customDelimiter) {
-    if (this.#has(customDelimiter)) {
-      this.#delimiters.push(customDelimiter);
-    }
-  }
-
-  #has(customDelimiter) {
-    return customDelimiter ? true : false;
+  add(customDelimiter) {
+    this.#delimiters.push(customDelimiter);
   }
 
   extract(userInputString) {

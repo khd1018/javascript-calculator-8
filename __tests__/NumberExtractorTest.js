c80a744c39b81ab85bc5f42704ed418a6203510a
hyeongdong <khd941018@gmail.com>
2025-10-19 19:31:44 +0900

    refactor(NumberExtractor): 중복 코드 제거

--- __tests__/NumberExtractorTest.js
+++ __tests__/NumberExtractorTest.js
@@ -1,8 +1,6 @@
 import NumberExtractor from "../src/NumberExtractor";
 
 describe("NumberExtractor test", () => {
-  const numberExtractor = new NumberExtractor();
-
   test.each([
     ["", "1,2,3", [1, 2, 3]],
     ["", "3:5:7", [3, 5, 7]],
@@ -10,6 +8,7 @@ describe("NumberExtractor test", () => {
     ["-", "2-4:6", [2, 4, 6]],
     [null, "1:2:34", [1, 2, 34]],
     [undefined, `1:2:34`, [1, 2, 34]],
+    ["-", "2:4:6", [2, 4, 6]],
   ])("문자열에서 숫자들을 분리할 수 있다.", (customDelimiter, strings, numbers) => {
     const numberExtractor = new NumberExtractor(customDelimiter);
 

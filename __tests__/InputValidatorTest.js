d0b0d02d2df5f2c7be5cad69fa4ed52ca3673683
hyeongdong <khd941018@gmail.com>
2025-10-20 00:34:32 +0900

    test(InputValidator): 입력값 검증 기능 테스트 구현

---
+++ __tests__/InputValidatorTest.js
@@ -0,0 +1,17 @@
+import InputValidator from "../src/services/InputValidator";
+
+describe("InputValidator test", () => {
+  const inputValidator = new InputValidator();
+
+  test("빈문자열을 입력한 경우 Error를 발생시킬 수 있다.", () => {
+    expect(() => inputValidator.checkEmpty("")).toThrow("[ERROR]");
+  });
+
+  test.each([[[1, -2, 3]], [[0, 1, 2, 3]]])("음수를 입력한 경우 Error를 발생시킬 수 있다.", (numbers) => {
+    expect(() => inputValidator.checkPositive(numbers)).toThrow("[ERROR]");
+  });
+
+  test.each([[[NaN]], [[NaN, NaN]], [[1, NaN]]])("문자열에 숫자가 없거나 잘못된 문자열을 입력한 경우 Error를 발생시킬 수 있다.", (numbers) => {
+    expect(() => inputValidator.checkPositive(numbers)).toThrow("[ERROR]");
+  });
+});

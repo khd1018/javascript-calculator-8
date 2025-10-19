0fde736e62a6a823eec346299962c9e310621b34
hyeongdong <khd941018@gmail.com>
2025-10-20 02:04:47 +0900

    feat(InputValidator): 커스텀 문자열 입력 부분 검증하는 기능 구현

--- src/services/InputValidator.js
+++ src/services/InputValidator.js
@@ -10,9 +10,29 @@ class InputValidator {
   checkPositive(numbers) {
     numbers.forEach((number) => {
       if (number <= 0 || Number.isNaN(number)) {
-        throw new Error("[ERROR]잘못된 값을 입력하셨습니다");
+        throw new Error("[ERROR]잘못된 값을 입력하셨습니다. 양수와 구분자를 정확히 입력해주세요.");
       }
     });
+
+    return;
+  }
+
+  checkCustomDelimiter(userInputString) {
+    const CUSTOM_DELIMITER_PATTERN = /^\/\/(.+)\n/;
+
+    if (!CUSTOM_DELIMITER_PATTERN.test(userInputString)) {
+      throw new Error("[ERROR] 잘못된 커스텀 구분자 형식입니다. (예: //;\\n1;2;3)");
+    }
+
+    return;
+  }
+
+  checkPrefix(userInputString) {
+    const NUMBER_PREFIX_PATTERN = /^[1-9]/;
+
+    if (!userInputString.startsWith("//") && !NUMBER_PREFIX_PATTERN.test(userInputString)) {
+      throw new Error("[ERROR] 입력 형식이 잘못되었습니다. 숫자 또는 // 로 시작해야 합니다.");
+    }
 
     return;
   }

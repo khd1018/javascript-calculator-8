507b75eee7464cc477249f671d35ef06758240c6
hyeongdong <khd941018@gmail.com>
2025-10-19 17:10:36 +0900

    test(NumberExtractor): 커스텀문자열 검증을 테스트할 수 있는 테스트 항목 추가 및 코드 수정

--- __tests__/NumberExtractorTest.js
+++ __tests__/NumberExtractorTest.js
@@ -4,10 +4,15 @@ describe("NumberExtractor test", () => {
   const numberExtractor = new NumberExtractor();
 
   test.each([
-    ["1,2,3", [1, 2, 3]],
-    ["3:5,7", [3, 5, 7]],
-    ["1;3,5", [1, 3, 5]],
-  ])("문자열에서 숫자들을 분리할 수 있다.", (strings, numbers) => {
+    ["", "1,2,3", [1, 2, 3]],
+    ["", "3:5:7", [3, 5, 7]],
+    [";", "1;3,5", [1, 3, 5]],
+    ["-", "2-4:6", [2, 4, 6]],
+    [null, "1:2:34", [1, 2, 34]],
+    [undefined, `1:2:34`, [1, 2, 34]],
+  ])("문자열에서 숫자들을 분리할 수 있다.", (customDelimiter, strings, numbers) => {
+    const numberExtractor = new NumberExtractor(customDelimiter);
+
     expect(numberExtractor.extract(strings)).toStrictEqual(numbers);
   });
 });

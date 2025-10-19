d50e95a251fa24dfc8e2907c9be9cb24f3fc9a25
hyeongdong <khd941018@gmail.com>
2025-10-20 02:55:45 +0900

    feat(CalculatorController): 문자열 숫자들의을 합을 구하는 기능 구현

---
+++ src/controllers/CalculatorController.js
@@ -0,0 +1,24 @@
+class CalculatorController {
+  constructor({ calculator, delimiterFinder, numberExtractor, inputValidator }) {
+    this.calculator = calculator;
+    this.delimiterFinder = delimiterFinder;
+    this.numberExtractor = numberExtractor;
+    this.inputValidator = inputValidator;
+  }
+
+  calculateSum(userInputString) {
+    this.inputValidator.checkEmpty(userInputString);
+    this.inputValidator.checkPrefix(userInputString);
+    this.inputValidator.checkCustomDelimiter(userInputString);
+
+    const customDelimiter = this.delimiterFinder.getDelimiter(userInputString);
+
+    if (customDelimiter && !this.numberExtractor.has(customDelimiter)) {
+      this.numberExtractor.add(customDelimiter);
+    }
+
+    const numbers = this.numberExtractor.extract(userInputString);
+    this.inputValidator.checkPositive(numbers);
+    return this.calculator.sum(numbers);
+  }
+}

6d57ab7c3d157881bc2a4f70899e59a80f6ec549
hyeongdong <khd941018@gmail.com>
2025-10-20 21:00:49 +0900

    refactor(DelimiterFinder): 사용하지않는 메서드 제거

--- src/services/DelimiterFinder.js
+++ src/services/DelimiterFinder.js
@@ -1,14 +1,6 @@
 class DelimiterFinder {
   #delimiterSearchRegex = /\/\/(.*?)\\n/;
 
-  constructor() {}
-
-  hasCustomDelimiter(userInputString) {
-    const CUSTOM_DELIMITER_PATTERN = /^\/\/(\S+)\n/;
-
-    return CUSTOM_DELIMITER_PATTERN.test(userInputString);
-  }
-
   getDelimiter(customDelimiterString) {
     const customDelimiter = customDelimiterString.match(this.#delimiterSearchRegex) || [];
 
